f4cad6b979d4d2a41940cc6272ab7a3e4ae3fc2d
dt3310321 <dutie123@qq.com>
2017-08-01 20:23:38 +0800

    Modify the Exception Module

--- qcloud_cos/cos_exception.py
+++ qcloud_cos/cos_exception.py
@@ -1,14 +1,65 @@
+import xml.dom.minidom
 
-class CosException(Exception):
+
+class COSException(Exception):
     def __init__(self, message):
         Exception.__init__(self, message)
 
 
-class UserDefinedError(CosException):
+class COSBadResponseError(COSException):
     def __init__(self, message):
-        CosException.__init__(self, message)
+        Exception.__init__(self, message)
 
 
-class ServerError(CosException):
+def digest_xml(data):
+    msg = dict()
+    try:
+        tree = xml.dom.minidom.parseString(data)
+        root = tree.documentElement
+
+        result = root.getElementsByTagName('Code')
+        msg['code'] = result[0].childNodes[0].nodeValue
+
+        result = root.getElementsByTagName('Message')
+        msg['message'] = result[0].childNodes[0].nodeValue
+
+        result = root.getElementsByTagName('Resource')
+        msg['resource'] = result[0].childNodes[0].nodeValue
+
+        result = root.getElementsByTagName('RequestId')
+        msg['requestid'] = result[0].childNodes[0].nodeValue
+
+        result = root.getElementsByTagName('TraceId')
+        msg['traceid'] = result[0].childNodes[0].nodeValue
+        return msg
+    except Exception as e:
+        raise COSBadResponseError("Response Error Msg Is INVALID")
+
+
+class ClientError(COSException):
     def __init__(self, message):
-        CosException.__init__(self, message)
+        COSException.__init__(self, message)
+
+
+class COSServiceError(COSException):
+    def __init__(self, message):
+        COSException.__init__(self, message)
+        self._msg = digest_xml(message)
+
+    def get_full_msg(self):
+        return self._msg
+
+    def get_error_code(self):
+        return self._msg['code']
+
+    def get_error_msg(self):
+        return self._msg['message']
+
+    def get_resource_location(self):
+        return self._msg['resource']
+
+    def get_trace_id(self):
+        return self._msg['requestid']
+
+    def get_request_id(self):
+        return self._msg['traceid']
