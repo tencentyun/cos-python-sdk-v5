1d32ff7fb332f34cdba3bd7ee7c33f909aa21694
tiedu <dutie123@qq.com>
2017-09-29 19:59:44 +0800

    Modify the Auth Module

--- qcloud_cos/cos_client.py
+++ qcloud_cos/cos_client.py
@@ -22,7 +22,7 @@ logging.basicConfig(
                 level=logging.INFO,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='cos_s3.log',
+                filename='cos_v5.log',
                 filemode='w')
 logger = logging.getLogger(__name__)
 reload(sys)
