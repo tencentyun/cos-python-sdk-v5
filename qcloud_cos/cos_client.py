37905f6ffaf0edb547222cb43cbab45bf1348321
tiedu <dutie123@qq.com>
2018-01-25 20:26:24 +0800

    remove default logging config

--- qcloud_cos/cos_client.py
+++ qcloud_cos/cos_client.py
@@ -21,12 +21,6 @@ from cos_threadpool import SimpleThreadPool
 from cos_exception import CosClientError
 from cos_exception import CosServiceError
 
-logging.basicConfig(
-                level=logging.INFO,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='cos_v5.log',
-                filemode='w')
 logger = logging.getLogger(__name__)
 reload(sys)
 sys.setdefaultencoding('utf-8')
