15f06bf821c453d2b42b60f55633aa6c27740e40
tiedu <dutie123@qq.com>
2018-06-24 17:33:53 +0800

    Update cos_comm.py
    fix re rule

--- qcloud_cos/cos_comm.py
+++ qcloud_cos/cos_comm.py
@@ -198,8 +198,8 @@ def format_region(region):
     if not region:
         raise CosClientError("region is required not empty!")
     region = to_unicode(region)
-    if not re.match('^[A-Za-z0-9][A-Za-z0-9\-]+[A-Za-z0-9]$', region):
-        raise CosClientError("region:%s format is illegal, only digit, letter and - is allowed!" % region)
+    if not re.match('^[A-Za-z0-9][A-Za-z0-9.\-]*[A-Za-z0-9]$', region):
+        raise CosClientError("region format is illegal, only digit, letter and - is allowed!")
     if region.find(u'cos.') != -1:
         return region  # 传入cos.ap-beijing-1这样显示加上cos.的region
     if region == u'cn-north' or region == u'cn-south' or region == u'cn-east' or region == u'cn-south-2' or region == u'cn-southwest' or region == u'sg':
@@ -233,8 +233,8 @@ def format_bucket(bucket, appid):
         raise CosClientError("bucket is not string")
     if not bucket:
         raise CosClientError("bucket is required not empty")
-    if not re.match('^[A-Za-z0-9][A-Za-z0-9\-]+[A-Za-z0-9]$', bucket):
-        raise  CosClientError("bucket:%s format is illegal, only digit, letter and - is allowed!" % bucket)
+    if not (re.match('^[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]$', bucket) or re.match('^[A-Za-z0-9]$', bucket)):
+        raise CosClientError("bucket format is illegal, only digit, letter and - is allowed!")
     # appid为空直接返回bucket
     if not appid:
         return to_unicode(bucket)
