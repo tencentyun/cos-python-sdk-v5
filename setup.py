6d925e1b3334ea571eebf4a2ab56de95a9c7a794
tiedu <dutie123@qq.com>
2018-09-28 20:42:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.5.7',
+    version='1.5.8',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
