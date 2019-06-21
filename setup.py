ae44495a0ebf00f7f9afe971776a9337301d3ded
tiedu <dutie123@qq.com>
2019-06-21 10:21:22 +0800

    Update setup.py
    publish 1.7.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.6.9',
+    version='1.7.0',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
