2c29bd64ff1809810750d899460e0e3ecd8553b5
yuniszhang <yuniszhang@tencent.com>
2021-10-11 20:00:37 +0800

    发布新版本

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.9.8',
+    version='1.9.9',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
