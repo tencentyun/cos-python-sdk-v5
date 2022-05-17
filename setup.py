cf5afbea67a00d281427c2d2f0f311ed5923952a
yuniszhang <yuniszhang@tencent.com>
2022-05-17 15:36:27 +0800

    add version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.9.15',
+    version='1.9.16',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
