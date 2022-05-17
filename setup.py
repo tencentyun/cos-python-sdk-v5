61610c74cd247a983f3f252c9911b64f8e8eab3d
yuniszhang <yuniszhang@tencent.com>
2022-05-17 20:37:46 +0800

    update sdk version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.9.16',
+    version='1.9.17',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
