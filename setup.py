accb2dab810417fbc0d318ba7cf74455c4949f99
yuniszhang <yuniszhang@tencent.com>
2022-05-31 11:47:24 +0800

    解决certifi新版本不支持python2的问题

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.9.17',
+    version='1.9.18',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
