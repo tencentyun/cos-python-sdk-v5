3052563623bde63b1644a70f5aa1e435ac82d89f
yuniszhang <yuniszhang@tencent.com>
2022-06-13 17:04:30 +0800

    解决requests和certifi组件不兼容python老版本问题

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 from platform import python_version_tuple
+import sys
 import io
 
 
 def requirements():
     with open('requirements.txt', 'r') as fileobj:
-        requirements = [line.strip() for line in fileobj]
+        requirements = []
+
+        # certifi2021.10.8之后的版本不再支持python2和python3.6之前的版本
+        if sys.version_info.major < 3 or \
+                (sys.version_info.major == 3 and sys.version_info.minor < 6):
+            requirements.append('certifi<=2021.10.8')
+
+        # requests2.27.1之后的版本不再支持python2和python3.7之前的版本
+        if sys.version_info.major < 3 or \
+                (sys.version_info.major == 3 and sys.version_info.minor < 7):
+            requirements.append('requests>=2.8,<=2.27.1')
+        else:
+            requirements.append('requests>=2.8')
+
+        requirements.extend([line.strip() for line in fileobj])
         return requirements
 
 
