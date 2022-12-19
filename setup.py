5c8f4534b816e38af4cf6e6f634581ec013a08c3
l-iberty <2593093242@qq.com>
2022-12-19 19:43:35 +0800

    1.9.22

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.9.21',
+    version='1.9.22',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
