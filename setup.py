3208b43150f2568405b15d7908f548f8a30aaca9
libertyzhu <libertyzhu@tencent.com>
2024-08-08 10:12:56 +0800

    1.9.31发布

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def long_description():
 
 setup(
     name='cos-python-sdk-v5',
-    version='1.9.30',
+    version='1.9.31',
     url='https://www.qcloud.com/',
     license='MIT',
     author='tiedu, lewzylu, channingliu',
